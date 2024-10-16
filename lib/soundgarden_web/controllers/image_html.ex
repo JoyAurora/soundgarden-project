1a05d2a00b994e7d3dab9eb9a7fba8156ebb7a0c
giallo <joyaurora@outlook.com>
2024-10-16 12:06:16 +0100

    defined places in html_image.exe

--- lib/soundgarden_web/controllers/image_html.ex
+++ lib/soundgarden_web/controllers/image_html.ex
@@ -4,10 +4,25 @@ defmodule SoundgardenWeb.ImageHTML do
   embed_templates "image_html/*"
 
   @doc """
-  Renders a image form.
+  Renders an image form.
   """
   attr :changeset, Ecto.Changeset, required: true
   attr :action, :string, required: true
+  attr :places, :list, default: []
 
-  def image_form(assigns)
+  def image_form(assigns) do
+    ~H"""
+    <form action={@action} method="post">
+      <!-- Form fields go here -->
+      <label for="place">Select a Place:</label>
+      <select name="place" id="place">
+        <%= for place <- @places do %>
+          <option value="<%= place.id %>"><%= place.name %></option>
+        <% end %>
+      </select>
+
+      <%= submit "Save" %>
+    </form>
+    """
+  end
 end
